12067d219998e04600f7dd944bbebd993d74cecf
Godwin <Godwinadeleke@gmail.com>
2018-04-25 11:33:12 +0100

    Bookstore Example

---
+++ src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,6 @@
+package com.pluralsight.bookstore.model;
+
+public enum Language {
+	ENGLISH,FRENCH,SPANISH,PORTUGESE,ITALLIAN,FINISH,GERMAN,DEUTSCH,RUSSIAN
+
+}
---
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
@@ -0,0 +1,46 @@
+package com.pluralsight.bookstore.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.pluralsight.bookstore.model.Book;
+
+public class BookRepository {
+	@PersistenceContext(unitName = "bookStorePU")
+	private EntityManager em;
+	
+	public Book find(Long id) {
+		return em.find(Book.class, id);
+		
+	}
+public Book create(Book book) {
+	em.persist(book);
+	return book;
+		
+	}
+public void  delete(Long id) {
+	em.remove(em.getReference(Book.class, id));
+	
+	
+}
+
+public List<Book> findAll() {
+TypedQuery<Book> query = em.createQuery( "SELECT b from Book b order by b.title", Book.class);
+return query.getResultList();
+	
+}
+
+public Long countAll() {
+	TypedQuery<Long> query = em.createQuery("SELECT count(b) from Book b", Long.class);
+	return query.getSingleResult();
+	
+}
+
+	
+}
+
+	
+
