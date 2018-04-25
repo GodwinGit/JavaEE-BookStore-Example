74ee825d9f4ec3844f17e8397437605407107b1a
Godwin <Godwinadeleke@gmail.com>
2018-04-25 12:04:00 +0100

    Updated

--- src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
@@ -5,9 +5,14 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
 
 import com.pluralsight.bookstore.model.Book;
-
+@Transactional(SUPPORTS)
 public class BookRepository {
 	@PersistenceContext(unitName = "bookStorePU")
 	private EntityManager em;
@@ -16,11 +21,13 @@ public class BookRepository {
 		return em.find(Book.class, id);
 		
 	}
+	@Transactional(REQUIRED)
 public Book create(Book book) {
 	em.persist(book);
 	return book;
 		
 	}
+	@Transactional(REQUIRED)
 public void  delete(Long id) {
 	em.remove(em.getReference(Book.class, id));
 	
